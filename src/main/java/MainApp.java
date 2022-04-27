dfaee306aa48a56d11535e8208f930059a2d20af
Prasun Kanti Howlader <prasunhowlader@yahoo.com>
2022-04-27 09:57:13 +0600

    Initial commit

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,28 @@
+import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
+
+public class MainApp extends SimpleApplication {
+    public static void main(String[] args) {
+        MainApp mainApp = new MainApp();
+        mainApp.start();
+    }
+    @Override
+    public void simpleInitApp() {
+        Box box = new Box(1,1,1);
+        Geometry geometry  = new Geometry("box",box);
+        Spatial spatial = assetManager.loadModel("Models/cone.obj");
+       // geometry.setMaterial(new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md"));
+        rootNode.attachChild(spatial);
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)));
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun);
+
+    }
+}
